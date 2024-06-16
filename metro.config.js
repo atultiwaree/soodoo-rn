103616a3cc1dfc5624387ea8f2c291885827fe33
Ginie Digital <giniedigitaldeveloper@gmail.com>
2024-06-17 03:33:37 +0530

    [Add]: Expo, [Test]: Expo library, Initial Commit

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,20 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {mergeConfig, getDefaultConfig} = require('@react-native/metro-config');
 
+const defaultConfig = getDefaultConfig(__dirname);
+const {resolver, transformer} = defaultConfig;
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    ...transformer, // <--- THIS WAS MISSING
+  },
+  resolver: {
+    ...resolver, // <--- THIS WAS MISSING
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
